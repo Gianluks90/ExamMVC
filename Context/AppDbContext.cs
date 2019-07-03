bea3a38a974f576d61facc26edd03668487ea112
Gianluks <gianluks90@gmail.com>
2019-07-04 00:30:35 +0200

    Initial commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ExamMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamMVC.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+
+        public AppDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services
+                .GetRequiredService<AppDbContext>();
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamMVC.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal Price { get; set; }
+        public string OrderCode { get; set; }
+        public DateTime ShippingDate { get; set; }
+
+        public Customer customer { get; set; }
+
+        public Order()
+        {
+
+        }
+
+        public Order(int id, DateTime creationDate, decimal price, string orderCode, DateTime shippingDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+            Price = price;
+            OrderCode = orderCode;
+            ShippingDate = shippingDate;
+        }
+    }
+}
